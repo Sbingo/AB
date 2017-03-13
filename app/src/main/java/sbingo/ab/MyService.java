f65615cad78c02b82357ca2f8f1376d3ca18fc95
bin <745677058@qq.com>
2017-03-13 23:27:09 +0800

    service init config

---
+++ app/src/main/java/sbingo/ab/MyService.java
@@ -0,0 +1,22 @@
+package sbingo.ab;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Author: Sbingo
+ * Date:   2017/03/13
+ */
+
+public class MyService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
