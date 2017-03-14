df6dbc555dbd264494ea672285569247f7caa6ee
Sbingo_666 <745677058@qq.com>
2017-03-14 18:00:06 +0800

    ready to analyze

--- app/src/main/java/sbingo/ab/MainActivity.java
+++ app/src/main/java/sbingo/ab/MainActivity.java
@@ -1,45 +1,136 @@
 package sbingo.ab;
 
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.os.Environment;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.List;
 
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements AccessibilityManager.AccessibilityStateChangeListener {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private Button serviceSwitch, start;
+    private TextView path;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
+    private AccessibilityManager accessibilityManager;
 
-    };
+    private final String TAG = this.getClass().getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        serviceSwitch = (Button) findViewById(R.id.service_switch);
+        start = (Button) findViewById(R.id.start);
+        path = (TextView) findViewById(R.id.path);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        accessibilityManager = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+        accessibilityManager.addAccessibilityStateChangeListener(this);
+
+        serviceSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "请点击老司机并开启", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if (isFileReady()) {
+                    startPackage("com.contextlogic.wish");
+//                }
+            }
+        });
+
+        updateStatus(isServiceEnabled() ? true : false);
     }
 
+    private boolean isFileReady() {
+        File dir = new File(Environment.getExternalStorageDirectory() + File.separator + "www");
+        File file = new File(dir, "rrr.txt");
+        if (file.exists() && file.length() > 0) {
+            if (!isFileUsed()) {
+                return true;
+            } else {
+                Toast.makeText(MainActivity.this, "目标文件已使用过，作业取消！", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "目标文件不存在或为空文件，无法作业！", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private boolean isFileUsed() {
+        // TODO: 2017/3/14 判断文件是否已使用过
+        return false;
+    }
+
+    @Override
+    public void onAccessibilityStateChanged(boolean enabled) {
+        Log.i(TAG, "onAccessibilityStateChanged ：" + enabled);
+        updateStatus(enabled);
+    }
+
+    private void updateStatus(boolean serviceEnabled) {
+        serviceSwitch.setEnabled(!serviceEnabled);
+        start.setEnabled(serviceEnabled);
+    }
+
+    private boolean isServiceEnabled() {
+        List<AccessibilityServiceInfo> accessibilityServices =
+                accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
+        for (AccessibilityServiceInfo info : accessibilityServices) {
+            if (info.getId().equals(getPackageName() + "/.MyService")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void startPackage(String packageName) {
+        PackageManager packageManager = getPackageManager();
+        if (packageExist(packageName)) {
+            Intent intent = packageManager.getLaunchIntentForPackage(packageName);
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this, "没有安装" + packageName, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 检查包是否存在
+     *
+     * @param packageName
+     * @return
+     */
+    private boolean packageExist(String packageName) {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return packageInfo != null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessibilityManager.removeAccessibilityStateChangeListener(this);
+    }
 }
--- app/src/main/java/sbingo/ab/MyService.java
+++ app/src/main/java/sbingo/ab/MyService.java
@@ -1,7 +1,12 @@
 package sbingo.ab;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
 
 /**
  * Author: Sbingo
@@ -10,13 +15,110 @@ import android.view.accessibility.AccessibilityEvent;
 
 public class MyService extends AccessibilityService {
 
+    private final String TAG = this.getClass().getName();
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.i(TAG, "onServiceConnected");
+    }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.i(TAG, "onAccessibilityEvent");
+        switch (event.getEventType()) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                Log.i(TAG, "TYPE_VIEW_CLICKED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+                Log.i(TAG, "TYPE_VIEW_LONG_CLICKED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
+                Log.i(TAG, "TYPE_VIEW_SELECTED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                Log.i(TAG, "TYPE_VIEW_FOCUSED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+                Log.i(TAG, "TYPE_VIEW_TEXT_CHANGED");
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                Log.i(TAG, "TYPE_WINDOW_STATE_CHANGED");
+                break;
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                Log.i(TAG, "TYPE_NOTIFICATION_STATE_CHANGED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+                Log.i(TAG, "TYPE_VIEW_HOVER_ENTER");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+                Log.i(TAG, "TYPE_VIEW_HOVER_EXIT");
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+                Log.i(TAG, "TYPE_TOUCH_EXPLORATION_GESTURE_START");
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                Log.i(TAG, "TYPE_WINDOW_CONTENT_CHANGED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+                Log.i(TAG, "TYPE_VIEW_SCROLLED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+                Log.i(TAG, "TYPE_VIEW_TEXT_SELECTION_CHANGED");
+                break;
+            case AccessibilityEvent.TYPE_ANNOUNCEMENT:
+                Log.i(TAG, "TYPE_ANNOUNCEMENT");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY:
+                Log.i(TAG, "TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY");
+                break;
+            case AccessibilityEvent.TYPE_GESTURE_DETECTION_START:
+                Log.i(TAG, "TYPE_GESTURE_DETECTION_START");
+                break;
+            case AccessibilityEvent.TYPE_GESTURE_DETECTION_END:
+                Log.i(TAG, "TYPE_GESTURE_DETECTION_END");
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_INTERACTION_START:
+                Log.i(TAG, "TYPE_TOUCH_INTERACTION_START");
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_INTERACTION_END:
+                Log.i(TAG, "TYPE_TOUCH_INTERACTION_END");
+                break;
+            case AccessibilityEvent.TYPE_WINDOWS_CHANGED:
+                Log.i(TAG, "TYPE_WINDOWS_CHANGED");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_CONTEXT_CLICKED:
+                Log.i(TAG, "TYPE_VIEW_CONTEXT_CLICKED");
+                break;
+            default:
+                Log.i(TAG, "default");
+        }
+        toRegister(event);
+        doRegister(event);
+    }
 
+    private void toRegister(AccessibilityEvent event) {
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
+            AccessibilityNodeInfo node = event.getSource();
+            List<AccessibilityNodeInfo> nodeList = node.findAccessibilityNodeInfosByText(" CREATE ACCOUNT ");
+            if (nodeList.size() > 0) {
+                nodeList.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+            node.recycle();
+        }
+    }
+
+    private void doRegister(AccessibilityEvent event) {
     }
 
     @Override
     public void onInterrupt() {
+        Log.i(TAG, "onInterrupt");
+    }
 
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "onUnbind");
+        return super.onUnbind(intent);
     }
 }
